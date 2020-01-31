7252eeb1373a5bb19bd2a25f164847fd494d3880
DougSherlock <dougthefiddler@hotmail.com>
2020-01-31 14:25:27 -0800

    Basic board layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [board, setBoard] = useState(
+  [
+    [{type:'City', id:0}, { type:'Road', closed:0, cities:[0, 1]}, {type:'City', id:1}, { type:'Road', closed:0, cities:[1, 2]}, {type:'City', id:2}],
+    [{type:'Road', closed:0, cities:[0, 3] }, { type:'Empty' }, {type:'Road', closed:0, cities:[1, 4]}, { type:'Empty' }, {type:'Road', closed:0, cities:[2, 5]}],
+    [{type:'City', id:0}, { type:'Road', closed:0, cities:[0, 1]}, {type:'City', id:1}, { type:'Road', closed:0, cities:[1, 2]}, {type:'City', id:2}],
+    [{type:'Road', closed:0, cities:[0, 3] }, { type:'Empty' }, {type:'Road', closed:0, cities:[1, 4]}, { type:'Empty' }, {type:'Road', closed:0, cities:[2, 5]}],
+    [{type:'City', id:0}, { type:'Road', closed:0, cities:[0, 1]}, {type:'City', id:1}, { type:'Road', closed:0, cities:[1, 2]}, {type:'City', id:2}]
+  ])
+
+  const traverse = () => {
+    alert('traverse called');
+  }
+  const onRoadClicked = (e) => {
+    let boardCopy = JSON.parse(JSON.stringify(board));
+    const row = e.target.getAttribute('row');
+    const col = e.target.getAttribute('col');
+    boardCopy[row][col].closed = !boardCopy[row][col].closed;
+    setBoard(boardCopy);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Road Builder Game </h1>        
+        <p>by Doug Sherlock</p>
+        <p>[Note that this game is still in development.]</p>
+      </div>
+      <div>
+      {/* <table className="Board">
+        <tr><td className="City">0</td><td className="Road">0:1</td><td className="City">1</td><td className="Road">1:2</td><td className="City">2</td></tr>
+        <tr><td className="Road">0:3</td><td></td><td className="Road">1:4</td><td></td><td className="Road">2:5</td></tr>
+        <tr><td className="City">3</td><td className="Road">3:4</td><td className="City">4</td><td className="Road">4:5</td><td className="City">5</td></tr>
+        <tr><td className="Road">3:6</td><td></td><td className="Road">4:7</td><td></td><td className="Road">5:8</td></tr>
+        <tr><td className="City">6</td><td className="Road">6:7</td><td className="City">7</td><td className="Road">7:8</td><td className="City">8</td></tr>
+      </table> */}
+      <table className="Board">
+        {
+          board.map((row, rowIndex) => {
+            return <tr>
+              {
+                row.map((item, colIndex) => {
+                  switch (item.type) 
+                  {
+                    case 'City':
+                      return <td className={item.type} row={rowIndex} col={colIndex} id={rowIndex+":"+colIndex}><span className="glyphicon glyphicon-home"></span> {item.id}</td>
+                    case 'Road':
+                      const cls = item.closed ? item.type + ' closed ' : item.type;                      
+                      return <td className={cls} 
+                        row={rowIndex} 
+                        col={colIndex} 
+                        id={rowIndex+":"+colIndex} 
+                        onClick={onRoadClicked}>
+                          <span className="glyphicon glyphicon-road"></span> {item.cities[0]+":"+item.cities[1]}
+                      </td>
+                    default:
+                      return <td className={item.type} row={rowIndex} col={colIndex} id={rowIndex+":"+colIndex} ></td>
+                    }
+                })
+              }
+              </tr>
+          })
+        }
+      </table>
+      </div>
+      <div>
+      <p><strong>Click on a road to mark it as broken or closed.</strong></p>
+      </div>
+      <div>
+      <p><strong>Click the 'Drive the Roads' button to determine if there are enough roads to connect all the cities.</strong></p>
+      <button onClick={traverse}>Drive the Roads</button>
+      </div>
     </div>
   );
 }
